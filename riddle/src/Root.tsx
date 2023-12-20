75c23d89d41ab596281c6e686136798833ac243f
Guillaume Pierson <guillaumep@marmelab.com>
2023-12-20 17:00:13 +0100

    fix link transition

--- riddle/src/Root.tsx
+++ riddle/src/Root.tsx
@@ -1,18 +1,18 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function Root() {
   return (
     <div className="w-screen h-screen bg-indigo-950 text-zinc-50">
       <div className="flex w-screen space-x-6 p-4 rounded-b-lg bg-indigo-900">
-        <Link to="/" unstable_viewTransition>
+        <NavLink to="/" unstable_viewTransition>
           Home
-        </Link>
-        <Link to="/riddle/1" unstable_viewTransition>
+        </NavLink>
+        <NavLink to="/riddle/1/question/0" unstable_viewTransition>
           Riddle 1
-        </Link>
-        <Link to="/riddle/2" unstable_viewTransition>
+        </NavLink>
+        <NavLink to="/riddle/2/question/0" unstable_viewTransition>
           OWASP TOP 3
-        </Link>
+        </NavLink>
       </div>
       <Outlet />
     </div>
